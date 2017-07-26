08ca7f52dd4691afb7969903c1fdbcd388432f35
李林 <ww1095@163.com>
2017-07-26 14:17:35 +0800

    init

---
+++ pages/index/index.js
@@ -0,0 +1,20 @@
+
+var app = getApp()
+
+Page({
+  data: {
+  },
+  templateView:function(){
+    wx.navigateTo({
+      url: '../login/login',
+    })
+    // wx.redirectTo({
+    //   url: '../login/login',
+    // })
+  }
+
+})
+
+
+
+
